55fc9f60ba51adfefeba4b5f36c3af49ee627873
azadeh-toreda <azadeh@toreda.com>
2021-02-16 16:39:43 -0800

    updated test string, item and string files

--- src/datasets/item.ts
+++ src/datasets/item.ts
@@ -2,3 +2,4 @@ export interface DatasetsItem<T> {
     label: string,
     value: T
 }
+export type DatasetItems<T> =Array <DatasetsItem<T>>;
--- src/suites/strings.ts
+++ src/suites/strings.ts
@@ -1,18 +1,31 @@
-export interface StringTest {
-    label: string;
-    input: any;
-    expectedResult: any;
-}
+import { EMPTY_STRING, HTML_UNESCAPED_INVALID, HTML_UNESCAPED_VALID, PADDED } from '../datasets/strings';
+
+import { DatasetItems } from '../datasets/item';
 
 export type StringFuncWrapper = (input: string) => any;
 
-export async function stringArgValidation(fn: StringFuncWrapper, val: StringTest) {
-    
-    describe('checking for empty strings', () => {
-        
-        it(val.label, () => {
-            expect(fn(val.input)).toEqual(val.expectedResult)
-        })
-    })
+export async function stringArgValidation(fn: StringFuncWrapper, expectedResult: any, dataSet: DatasetItems<string>, label: string) {
 
+    describe(label, () => {
+        for (let data of dataSet) {
+            it(data.label, () => {
+                expect(fn(data.value)).toEqual(expectedResult);
+            })
+        }
+    })
 }
+export async function emptySuite(fn: StringFuncWrapper, expectedResult: any) {
+    stringArgValidation(fn, expectedResult, EMPTY_STRING, "empty string suite")
+}
+
+export async function padded(fn: StringFuncWrapper, expectedResult: any) {
+    stringArgValidation(fn, expectedResult, PADDED, "padded suite")
+}
+export async function validHTML(fn: StringFuncWrapper, expectedResult: any){
+    stringArgValidation(fn, expectedResult, HTML_UNESCAPED_VALID, "valid HTML suite")
+}
+export async function unvalidHTML(fn: StringFuncWrapper, expectedResult: any){
+    stringArgValidation(fn, expectedResult, HTML_UNESCAPED_INVALID, "invalid HTML suite")
+}
+
+
